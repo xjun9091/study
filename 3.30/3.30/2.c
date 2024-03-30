aeec2db670b375a0f3790bd577ce26f8ec633b4d
Not defined1588 <3112604846@qq.com>
2024-03-30 13:09:05 +0000

    update 3.30/3.30/2.c.
    Signed-off-by: Not defined1588 <3112604846@qq.com>

--- 3.30/3.30/2.c
+++ 3.30/3.30/2.c
@@ -5,7 +5,7 @@ int main()
 	int a = 0;
 	int b = 0;
 	int sum=a+b;
-	scanf ("%d %d",&a ,&b);//scanf_s可以运行没有警告
-	printf("sum=%d\n", sum=a+b);
+	scanf ("%d %d",&a ,&b);//scanf_s没芯
+	printf("%d\n", sum);
 	return 0;
 }
