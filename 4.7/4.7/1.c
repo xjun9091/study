4c1535913f08783f6bff56d3a1843e57b0775c54
not-defined1588 <3112604846@qq.com>
2024-04-07 23:16:54 +0800

    4.7

--- 4.7/4.7/1.c
+++ 4.7/4.7/1.c
@@ -1,22 +1,62 @@
-#define _CRT_SECURE_NO_WARNINGS 
+//#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	for (i = 100; i <= 200; i++)
+//	{
+//		int n = 0;
+//		int flag = 1;
+//		for (n = 2; n <= i - 1; n++)
+//		{
+//			if (i % n == 0)
+//			{
+//				flag = 0;
+//				break;
+//			}
+//		}
+//			if(flag == 1)
+//				printf("%d ", i);
+//	}
+//	return 0;
+//}
+
+//
+//#include <stdio.h>
+//int main() {
+//	int x = 3;
+//	int y = 3;
+//	switch (x % 2) 
+//	{
+//	case 1:
+//		switch (y)
+//		{
+//		case 0:
+//			printf("first");
+//		case 1:
+//			printf("second");
+//			break;
+//		default: printf("hello");
+//		}
+//	case 2:
+//		printf("third");
+//	}
+//	return 0;
+//}
 #include <stdio.h>
 int main()
 {
-	int i = 0;
-	for (i = 100; i <= 200; i++)
+	int a = 0, b = 0;
+	for (a = 1, b = 1; a <= 100; a++)
 	{
-		int n = 0;
-		int flag = 1;
-		for (n = 2; n <= i - 1; n++)
+		if (b >= 20) break;
+		if (b % 3 == 1)
 		{
-			if (i % n == 0)
-			{
-				flag = 0;
-				break;
-			}
+			b = b + 3;
+			continue;
 		}
-			if(flag == 1)
-				printf("%d ", i);
+		b = b - 5;
 	}
+	printf("%d\n", a);
 	return 0;
 }
