b39bc634c15192a317a7ecdbe03f42dc68c36b08
not-defined1588 <3112604846@qq.com>
2024-04-09 20:12:28 +0800

    4.9

--- 4.9/4.9/2.c
+++ 4.9/4.9/2.c
@@ -1,4 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS 
+
+//输出99乘法口诀表
+
+
 #include <stdio.h>
 //int main()
 //{
@@ -43,17 +47,17 @@
 //	return 0;
 //}
 
-int main()
-{
-	int a = 0;
-	int b = 0;
-	for (a = 1; a < 10; a++)
-	{
-		for (b = 1; b <= a; b++)
-		{
-			printf("%d*%d=%d ", a, b, a * b);
-		}
-		printf("\n");
-	}
-	return 0;
-}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	for (a = 1; a < 10; a++)
+//	{
+//		for (b = 1; b <= a; b++)
+//		{
+//			printf("%d*%d=%2d ", a, b, a * b);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
