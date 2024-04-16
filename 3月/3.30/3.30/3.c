bc9b27d65f3a65dfe956b15af0d46e48495c9926
not-defined1588 <3112604846@qq.com>
2024-04-16 21:50:44 +0800

    3月

---
+++ 3月/3.30/3.30/3.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include<stdio.h>
+int main()
+{
+	printf("%zd\n", sizeof(int));//用%d可以运作，但会有警告
+	printf("%zd\n", sizeof(char));
+	printf("%zd\n", sizeof(long));
+	printf("%zd\n", sizeof(double));
+	printf("%zd\n", sizeof(float));
+	return 0;
+}
