5ab54c7a56443070268d2decd0cb201022a727a8
not-defined1588 <3112604846@qq.com>
2024-04-16 21:47:07 +0800

    4.16

--- 4.1/4.1/5.c
+++
@@ -1,8 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS 
-#include <stdio.h>
-int main()
-{
-	int x, y, z;
-
-	return 0;
-}
--- 4.2/4.2/2.c
+++
@@ -1,9 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS 
-#include<stdio.h>
-int main()
-{
-	int a, b,c;
-	while (scanf("%d %d", &a, &b)!=EOF)
-		printf("%d\n", c = a + b);
-	return 0;
-}
