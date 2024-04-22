de0185d121ff88db1c93c579ed7a3de03391493b
not-defined1588 <3112604846@qq.com>
2024-04-22 22:53:43 +0800

    4.22

--- 4月/Week 4/4.22/4.22/递归实现n的k次方.c
+++ 4月/Week 4/4.22/4.22/递归实现n的k次方.c
@@ -1,21 +1,21 @@
 #define _CRT_SECURE_NO_WARNINGS 
-#include <stdio.h>
-//编写一个函数实现n的k次方，使用递归实现。
-int nk(int x, int y)
-{
-	if (y == 0)
-		return 1;
-	else
-	   return x * nk(x, y - 1);
-}
-int main()
-{
-	int n = 0;
-	int k = 0;
-	while (scanf("%d %d", &n, &k) != EOF)
-	{
-	int r = nk(n, k);
-	printf("%d的%d次方为%d\n",n,k,r);
-	}
-	return 0;
-}
+//#include <stdio.h>
+////编写一个函数实现n的k次方，使用递归实现。
+//int nk(int x, int y)
+//{
+//	if (y == 0)
+//		return 1;
+//	else
+//	   return x * nk(x, y - 1);
+//}
+//int main()
+//{
+//	int n = 0;
+//	int k = 0;
+//	while (scanf("%d %d", &n, &k) != EOF)
+//	{
+//	int r = nk(n, k);
+//	printf("%d的%d次方为%d\n",n,k,r);
+//	}
+//	return 0;
+//}
