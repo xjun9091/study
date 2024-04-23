78c0cd423ef12c138feb780b73b65a21c832110a
not-defined1588 <3112604846@qq.com>
2024-04-24 00:17:20 +0800

    4.24

--- 4月/Week 4/4.23/4.23/青蛙怕楼梯.c
+++ 4月/Week 4/4.23/4.23/青蛙怕楼梯.c
@@ -4,13 +4,13 @@
 //斐波那契数列
 #include <stdio.h>
 
-int meth(int n)
+int meth(int n)//台阶
 {
-	int step = 0;
-	if (n == 1)
+	int step = 0;//步数
+	if (n == 1||n == 0)
 		return 1;
-	else 
-
+	else
+		return meth(n - 1) + meth(n - 2);
 }
 
 int main()
@@ -21,3 +21,36 @@ int main()
 	printf("小青蛙上%d层台阶有点%d种方法\n", n, m);
 	return 0;
 }
+
+//#include <stdio.h>  
+//
+//// 使用递归函数计算跳法数量  
+//int frogJump(int n) {
+//    if (n <= 0) {
+//        return 0;
+//    }
+//    else if (n == 1) {
+//        // 只有一层台阶时，只有一种跳法  
+//        return 1;
+//    }
+//    else if (n == 2) {
+//        // 有两层台阶时，可以一次跳1级跳两次，或者一次跳2级，共2种跳法  
+//        return 2;
+//    }
+//    else {
+//        // 对于n层台阶，小青蛙可以从n-1层跳1级上来，也可以从n-2层跳2级上来  
+//        // 因此，n层台阶的跳法数量是n-1层台阶和n-2层台阶跳法数量的和  
+//        return frogJump(n - 1) + frogJump(n - 2);
+//    }
+//}
+//
+//int main() {
+//    int n;
+//    printf("请输入台阶的阶数n：");
+//    scanf("%d", &n);
+//
+//    int ways = frogJump(n);
+//    printf("小青蛙跳到最后一层（第%d层）的不同跳法数量为：%d\n", n, ways);
+//
+//    return 0;
+//}
